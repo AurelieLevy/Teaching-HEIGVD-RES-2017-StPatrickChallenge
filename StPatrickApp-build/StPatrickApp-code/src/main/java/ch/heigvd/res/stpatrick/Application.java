7141d9152fe7663071b8bf8718ff1f0dadd083ff
Aurelie Levy <aurelie.levy@heig-vd.ch>
2017-03-20 12:40:03 +0100

    rendu. Seuls les premiers tests passent...

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
@@ -1,19 +1,79 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.Socket;
 
 /**
- * 
+ *
  * @author Olivier Liechti
  */
 public class Application {
 
-  IStreamProcessorsFactory processorsFactory = new StreamProcessorsFactory();
-  
-  public IStreamProcessorsFactory getStreamProcessorsFactory() {
-    return processorsFactory;
-  }
-  public static void main(String[] args) {
-      
-  }
+    BufferedWriter bw;
+    IStreamProcessorsFactory processorsFactory = new StreamProcessorsFactory();
+
+    public IStreamProcessorsFactory getStreamProcessorsFactory() {
+        return processorsFactory;
+    }
+
+    public static void main(String[] args) {
+        Socket clientSocket = null;
+        OutputStream os = null;
+        InputStream is = null;
+        try {
+            clientSocket = new Socket("10.192.114.100", 1703);
+            os = clientSocket.getOutputStream();
+            is = clientSocket.getInputStream();
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    IStreamDecoratorController getStreamDecoratorController() {
+        
+        /*IStreamDecoratorController decoController = new IStreamDecoratorController() {
+            @Override
+            public Reader decorateReader(Reader inputReader) {
+                BufferedReader br = new BufferedReader(inputReader);
+                try {
+                    int c = br.read();
+                    while (c != -1) {
+                        switch (c) {
+                            case 'a':
+                                break;
+                            case 'A':
+                                break;
+                            default:
+                                bw.write(c);
+                                break;
+                        }
+                        c = br.read();
+                    }
+                    bw.flush();
+                } catch (IOException e) {
+
+                }
+                return br;
+                //throw new UnsupportedOperationException("Not  yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Writer decorateWriter(Writer outputWriter) {
+                bw = new BufferedWriter(outputWriter);
+                return bw;
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+        return decoController;*/
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            
+    }
 
 }
--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
@@ -23,10 +25,24 @@ public class StreamProcessorsFactory implements IStreamProcessorsFactory {
             @Override
             public void process(Reader in, Writer out) throws IOException {
                 //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                
-            
+                BufferedReader br = new BufferedReader(in);
+                BufferedWriter bw = new BufferedWriter(out);
+                int c = br.read();
+                while (c != -1) {
+                    switch (c) {
+                        case 'e':
+                            break;
+                        case 'E':
+                            break;
+                        default:
+                            out.write(c);
+                            break;
+                    }
+                    c = br.read();
+                }
+                bw.flush();
             }
-        }
+        };
         return processor;
 
     }
